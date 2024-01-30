b948ba95b7a210f6786e4f64452770891b013e61
ADITYA VERMA <adityav1304@gmail.com>
2024-01-30 22:17:15 +0530

    a

--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,16 @@ export default authMiddleware({
     '/',
     '/events/:id',
     '/api/webhook/clerk',
+    '/api/v1/hooks/clerk',
     '/api/webhook/stripe',
-    '/api/uploadthing'
+    '/api/uploadthing',
+    '/api/v1/'
   ],
   ignoredRoutes: [
+    '/events/:id',
+    '/api/v1/',
+    '/api/v1/hooks/clerk',
+    '/api/webhook/clerk',
     '/api/webhook/stripe',
     '/api/uploadthing'
   ]
