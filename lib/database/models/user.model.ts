8f238abd3cecd61870655af5d1b4ba78b51fab24
ADITYA VERMA <adityav1304@gmail.com>
2024-01-30 22:25:14 +0530

    a

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -3,11 +3,20 @@ import { Schema, model, models } from "mongoose";
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
+  username: { type: String, unique: true }, // Adjusted based on the request JSON
   firstName: { type: String, required: true },
-  lastName: {type: String, required: true },
+  lastName: { type: String, required: true },
   photo: { type: String, required: true },
+  birthday: { type: String, default: '' }, // Added based on the request JSON
+  createdAt: { type: Date, default: Date.now }, // Added based on the request JSON
+  imageUrl: { type: String }, // Adjusted based on the request JSON
+  gender: { type: String, default: '' }, // Added based on the request JSON
+  lastSignInAt: { type: Date }, // Adjusted based on the request JSON
+  passwordEnabled: { type: Boolean, default: true }, // Added based on the request JSON
+  twoFactorEnabled: { type: Boolean, default: false }, // Added based on the request JSON
+  // Other fields can be added based on your application requirements
 })
+
 
 const User = models.User || model('User', UserSchema);
 
