f98b60d1d4038f4477fd7916167852a66c524ae3
ADITYA VERMA <adityav1304@gmail.com>
2024-02-01 01:50:28 +0530

    a

--- src/pages/landing/Signup.js
+++ src/pages/landing/Signup.js
@@ -1,26 +1,26 @@
-// import React from 'react'
-// import Button from '../../components/Button'
-// import SignupLogic from '../../Logic/UserLogic.js/Signup.logic'
-// import Input from '../../components/Input';
+import React from 'react'
+import Button from '../../components/Button'
+import SignupLogic from '../../Logic/UserLogic.js/Signup.logic'
+import Input from '../../components/Input';
 
-// function Signup() {
+function Signup() {
 
-//     const { inputs, validateMessage, signUpUser, signingin, navigateToPhone } = SignupLogic();
+    const { inputs, validateMessage, signUpUser, signingin, navigateToPhone } = SignupLogic();
 
     
     
     
 
-//   return (
-//     <form onSubmit={signUpUser} className='w-full'>
-//         {
-//             inputs.map((input, index) => (
-//                 <Input {...input} show={true}/>
-//             ))
-//         }
-//         <Button loading={signingin} type={'submit'} text='Sign up' style={'my-2'} />
-//     </form>
-//   )
-// }
+  return (
+    <form onSubmit={signUpUser} className='w-full'>
+        {
+            inputs.map((input, index) => (
+                <Input {...input} show={true}/>
+            ))
+        }
+        <Button loading={signingin} type={'submit'} text='Sign up' style={'my-2'} />
+    </form>
+  )
+}
 
-// export default Signup
+export default Signup
