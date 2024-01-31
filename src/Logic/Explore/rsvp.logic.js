1a692f2d004097e01485bffc6a883b4cfff67ddb
ADITYA VERMA <adityav1304@gmail.com>
2024-02-01 02:41:35 +0530

    Update rsvp.logic.js

--- src/Logic/Explore/rsvp.logic.js
+++ src/Logic/Explore/rsvp.logic.js
@@ -71,7 +71,6 @@ export default function RsvpLogic(event) {
         `${process.env.REACT_APP_WEBSITE_URL}/accept-invite/${eventId}`,
         email,
         userId,
-        "",
         name
       );
       
