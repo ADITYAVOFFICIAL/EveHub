55592b29502bc6a92ad1c3bf84934d043f1beea1
ADITYA VERMA <adityav1304@gmail.com>
2024-02-13 10:24:05 +0530

    a

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,43 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { footerLinks } from '../static/footerLinks';
+import React from 'react'
+import { Link, NavLink , useLocation} from 'react-router-dom'
+import { footerLinks } from '../static/footerLinks'
 
 function Footer() {
-  const location = useLocation();
-  const [visitorCount, setVisitorCount] = useState(0);
-
-  useEffect(() => {
-    // Call the serverless function to increment the visitor count
-    fetch('/api/incrementVisitorCount')
-      .then(response => response.json())
-      .then(data => {
-        // Update the visitor count
-        setVisitorCount(data.visitorCount);
-      })
-      .catch(error => {
-        console.error('Error incrementing visitor count:', error);
-      });
-  }, []); // Empty dependency array to only run this effect once on component mount
-
+    const location = useLocation(); 
   return (
     <div className='flex flex-col gap-4 md:gap-8 py-12 bg-secondary text-neutral-200 font-poppins border-t border-neutral-400' style={
-      location.pathname !== "/"
-        ? { borderRadius: "25px 25px 0 0" }
-        : {}
-    }>
-      <ul className='md:w-full flex flex-row flex-wrap items-center justify-center gap-4'>
-        {footerLinks.map((link, index) =>
-          <li className='border flex items-center justify-center rounded-full p-2 text-2xl hover:scale-125 transition-all' key={index}>
+        location.pathname !== "/"
+          ? { borderRadius: "25px 25px 0 0" }
+          : {}
+      }>
+    {/* <div className='w-full flex flex-row items-start uppercase gap-4 justify-between container'>
+        <div className='flex flex-col items-start gap-2'>
+            <Brand/>
+            <p className='font-medium capitalize text-md '>Subtitle Lorem Ipsum!</p>
+        </div>
+        <ul className='flex flex-col items-start gap-1'> 
+            <NavLink  className='font-light text-sm ' to={'/explore'}>Explore</NavLink>
+            <NavLink  className='font-light text-sm ' to={`/dashboard`}>Dashboard</NavLink>
+        </ul>
+    </div> */}
+    {/* <hr className='bg-neutral-100 border border-neutral-100 opacity-100'></hr> */}
+    <ul className='md:w-full flex flex-row flex-wrap items-center justify-center gap-4'>
+        {footerLinks.map((link, index) => 
+        <li className='border flex items-center justify-center rounded-full p-2 text-2xl hover:scale-125 transition-all'>
             <Link className='' target={'_blank'} to={link.link} title={link.title}>{link.icon}</Link>
-          </li>
+        </li>
         )}
-      </ul>
-      <div>
-        <p className='text-neutral-300 text-xs md:text-sm text-center'>Visitor Count: {visitorCount}</p>
+        
+    </ul>
+    <div>
         <p className='text-neutral-300 text-xs md:text-sm text-center'>&copy; EveHub {new Date().getFullYear()} | All rights reserved | <Link className='hover:text-accent hover:underline' target='_blank' to='https://github.com/ADITYAVOFFICIAL'>ADITYA VERMA</Link> | <Link className='hover:text-accent hover:underline' to='/dashboard'>For Organisers</Link></p>
-      </div>
+    </div>
     </div>
   )
 }
 
-export default Footer;
+export default Footer
